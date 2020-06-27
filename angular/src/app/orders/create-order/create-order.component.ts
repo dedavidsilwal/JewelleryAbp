12fb3c22e1779f44577fb82040471d5779ff1c80
David Silwal <de.davidsilwal@gmail.com>
2020-06-27 22:39:55 +0545

    create-order

--- angular/src/app/orders/create-order/create-order.component.ts
+++ angular/src/app/orders/create-order/create-order.component.ts
@@ -5,78 +5,57 @@ import {
   EventEmitter,
   Output,
 } from '@angular/core';
-import { finalize } from 'rxjs/operators';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import * as _ from 'lodash';
 import { AppComponentBase } from '@shared/app-component-base';
-import { OrderDto, OrderServiceProxy, OrderDetailDto, CustomerServiceProxy, MetalTypeServiceProxy, MetalTypeDto, MetalTypeDtoPagedResultDto, CreateOrderDto } from '@shared/service-proxies/service-proxies';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   templateUrl: './create-order.component.html',
 })
-export class CreateOrderComponent extends AppComponentBase
-  implements OnInit {
+export class CreateOrderComponent extends AppComponentBase implements OnInit {
   saving = false;
-  order = new CreateOrderDto();
-
-  metaltypes = new Array<MetalTypeDto>();
-
-  orderDetails: OrderDetailDto[] = [];
 
   @Output() onSave = new EventEmitter<any>();
 
+  orderForm: FormGroup;
+  orderDetails: FormArray;
+
+  orderFormValueChanges$;
+
   constructor(
     injector: Injector,
-    private _orderService: OrderServiceProxy,
-    private _customerService : CustomerServiceProxy,
-    private _metalTypeService : MetalTypeServiceProxy,
-    public bsModalRef: BsModalRef
+    public bsModalRef: BsModalRef,
+    private formBuilder: FormBuilder
   ) {
     super(injector);
   }
 
   ngOnInit(): void {
-
-    // this._metalTypeService.getAll()
-    // .subscribe((result: MetalTypeDtoPagedResultDto) => {
-    //   this.metaltypes = result.items;
-    // });
-
+    this.orderForm = this.formBuilder.group({
+      customerId: '',
+      requiredDate: '',
+      orderDetails: this.formBuilder.array([this.createOrderDetailForm()]),
+    });
   }
 
-  addOrderDetail(e): void{
-
-    e.preventDefault();
-    this.orderDetails.push(new OrderDetailDto());
+  createOrderDetailForm(): FormGroup {
+    return this.formBuilder.group({
+      productId: '',
+      quantity: '',
+      weight: '',
+      makingCharge: '',
+      wastage: '',
+      metalType: '',
+      metalCostThisDay: '',
+      unitPrice: '',
+      totalPrice: ''
+    });
   }
 
+  createOderDetail(): void {
+    this.orderDetails = this.orderForm.get('orderDetails') as FormArray;
 
-  removeOrderDetail(entry:OrderDetailDto):void{
-    // delete this.orderDetails[index];
-
-    const index = this.orderDetails.indexOf(entry, 0);
-if (index > -1) {
-  this.orderDetails.splice(index, 1);
-}
-  }
-
-  save(): void {
-    this.saving = true;
-
-    const order = new CreateOrderDto();
-    order.init(this.order);
-
-    this._orderService
-      .create(order)
-      .pipe(
-        finalize(() => {
-          this.saving = false;
-        })
-      )
-      .subscribe(() => {
-        this.notify.info(this.l('SavedSuccessfully'));
-        this.bsModalRef.hide();
-        this.onSave.emit();
-      });
+    this.orderDetails.push(this.createOrderDetailForm());
   }
 }
